7f5db4f32d572594f9014baa94e0c0b520e721af
webMaster <yegor_ka2004@ukr.net>
2019-10-27 22:30:15 +0200

    immit commit

--- script.js
+++ script.js
@@ -5,17 +5,11 @@ for(let x = 0;x < btn.length;x++){
   btn[x].addEventListener('click', () => {
     if(btn[x].className == "arrow down"){
       btn[x].classList.remove('down');
-
-      let text = document.createElement('div');
-      text.className = 'lorem_two';
-      text.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati nam non alias officia provident et commodi earum sint quia, rem sapiente at atque adipisci fuga. Adipisci illum ad eos voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati nam non alias officia provident et commodi earum sint quia, rem sapiente at atque adipisci fuga. Adipisci illum ad eos voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati nam non alias officia provident et commodi earum sint quia, rem sapiente at atque adipisci fuga. Adipisci illum ad eos voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati nam non alias officia provident et commodi earum sint quia, rem sapiente at atque adipisci fuga. Adipisci illum ad eos voluptatem.';
-      div[x].append(text);
+      div[x].style.display = 'block';
     }
     else{
       btn[x].classList.add('down');
-
-      let lorem = div[x].querySelector('.lorem_two');
-      lorem.remove();
+      div[x].style.display = 'none';
     }
   });
 }
