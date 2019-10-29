b1b960d27d8b0bcc7c29b99732c400ea523f9aa9
webMaster <yegor_ka2004@ukr.net>
2019-10-29 23:14:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let btn = document.querySelectorAll('.arrow');
 let div = document.querySelectorAll('.div_text');
 
 for(let x = 0;x < btn.length;x++){
-  btn[x].addEventListener('click', () => {
+  btn[x].addEventListener('click', function(){
     if(btn[x].className == "arrow down"){
       for(let y = 0;y < btn.length;y++){
         if(x != y){
