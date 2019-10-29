0675b524f1eb21496062e852f6aa6841e88ad75b
webMaster <yegor_ka2004@ukr.net>
2019-10-29 20:00:26 +0200

    immit commit

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let div = document.querySelectorAll('.div_text');
 for(let x = 0;x < btn.length;x++){
   btn[x].addEventListener('click', () => {
     if(btn[x].className == "arrow down"){
+      for(let y = 0;y < btn.length;y++){
+        if(x != y){
+          btn[y].classList.add('down');
+          div[y].style.display = 'none';
+        }
+      }
+
       btn[x].classList.remove('down');
       div[x].style.display = 'block';
     }
